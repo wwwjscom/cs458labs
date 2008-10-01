2af9d60e38450ab8cfd7d17ae5b1b8c8a4d40585
Jason Soo <wwwjscom@gmail.com>
2008-10-01 01:30:17 -0500

    Cleaning some things up

--- lab2/gen.rb
+++ lab2/gen.rb
@@ -1,29 +1,19 @@
 #!/usr/bin/env ruby -w
-load "main.rb"
+load "myLib.rb"
 
 size = ARGV[0].to_i
 
 a = 2
 
 begin
-	#puts a
 	if a.bitsize == size
-		#puts "=="*50
-		if a.prime?
-			puts "*"*10000000000000
-		end
 		a = a + 2
 	elsif a.bitsize > size
-		#puts "gt"
 		a /= 2 + 1
 	else
-		#puts "lt"
-		#a = a ** 2
 		a = a * 5
 	end
 
-#end while !a.prime? && a.bitsize != size
-#end while !a.prime?
 end while a.bitsize != size
 
 begin
@@ -31,7 +21,3 @@ begin
 end while !a.prime?
 
 puts a 
-
-puts a.prime?
-
-puts a.bitsize
---
+++ lab2/myLib.rb
@@ -0,0 +1,66 @@
+#!/usr/bin/env ruby -w
+
+input = ARGV[0].to_i
+
+class Integer
+
+    def bytesize
+    n = self
+    i=0
+    while n > 0
+      n = n >> 8
+      i += 1
+    end
+    return i
+  end
+
+  def bitsize
+    n = self
+    i=0
+    while n > 0
+      n = n >> 1
+      i += 1
+    end
+    return i
+  end
+
+ def prime?
+     n = self.abs()
+     return true if n == 2
+     return false if n == 1 || n & 1 == 0
+
+     return false if n > 3 && n % 6 != 1 && n % 6 != 5 
+
+     d = n-1
+     d >>= 1 while d & 1 == 0
+     20.times do                             
+       a = rand(n-2) + 1
+       t = d
+       y = ModMath.pow(a,t,n)             
+       while t != n-1 && y != 1 && y != n-1
+         y = (y * y) % n
+         t <<= 1
+       end
+       return false if y != n-1 && t & 1 == 0
+     end
+     return true
+   end
+
+end
+ 
+module ModMath
+
+   def ModMath.pow(base, power, mod)
+     result = 1
+     while power > 0
+       result = (result * base) % mod if power & 1 == 1
+       base = (base * base) % mod
+       power >>= 1;
+     end
+     result
+   end
+
+end
+
+
+p input.prime?
