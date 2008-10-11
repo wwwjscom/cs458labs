af70b80245fc6b82293dd6b3137838f89c15c80b
Jason Soo <wwwjscom@gmail.com>
2008-10-11 17:09:30 -0500

    cleaned it up

--- labs/lab2/code/keygen.rb
+++ labs/lab2/code/keygen.rb
@@ -17,8 +17,6 @@ while gcd.gcd(e, totient) != 1
 	e += 1
 end
 
-puts e
-
 # To find d, iterate over all
 # ints from 1 to totient. Test
 # d * e = 1 * mod(totient), that
@@ -29,14 +27,5 @@ while ((d * e) -1 ) % totient != 0
 	d += 1
 end
 
-puts d
-
 puts "Public key: (#{n}, #{e})"
 puts "Private key: (#{n}, #{d})"
-
-#end while gcd.gcd(e, totient) != 1
-#puts gcd.gcd(e, totient)
-
-#de = 1 (mod totient) = de = 1 + k * totient
-
-#puts e
