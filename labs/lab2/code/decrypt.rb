716a556e5997dd9d6dc3bf2603bc8fd2de87d505
Jason Soo <wwwjscom@gmail.com>
2008-10-13 23:19:16 -0500

    Going to clean it up a little now

--- labs/lab2/code/decrypt.rb
+++ labs/lab2/code/decrypt.rb
@@ -6,7 +6,6 @@ n = ARGV[0].to_i
 d = ARGV[1].to_i
 m = ARGV[2].to_i
 
-#plaintext = (m ** d) % n
 plaintext = Math.powmod(m, d, n)
 
 puts plaintext
--- labs/lab2/code/encrypt.rb
+++ labs/lab2/code/encrypt.rb
@@ -6,7 +6,6 @@ n = ARGV[0].to_i
 e = ARGV[1].to_i
 m = ARGV[2].to_i
 
-#cypher = (m ** e) % n
 cypher = Math.powmod(m, e, n)
 
 puts cypher
