6d7a8cc04984912f4316927419c989b2e9aadace
Jason Soo <wwwjscom@gmail.com>
2008-10-13 22:44:53 -0500

    IT WORKS

---
+++ labs/lab3/code/myLib.rb
@@ -0,0 +1,213 @@
+#!/usr/bin/env ruby -w
+
+class Integer
+
+    def bytesize
+    n = self
+    i=0
+    while n > 0
+      n = n >> 8
+      i += 1
+    end
+    return i
+  end
+
+  def bitsize
+    n = self
+    i=0
+    while n > 0
+      n = n >> 1
+      i += 1
+    end
+    return i
+  end
+
+ def prime?
+     n = self.abs()
+     return true if n == 2
+     return false if n == 1 || n & 1 == 0
+
+     return false if n > 3 && n % 6 != 1 && n % 6 != 5 
+
+     d = n-1
+     d >>= 1 while d & 1 == 0
+     20.times do                             
+       a = rand(n-2) + 1
+       t = d
+       y = ModMath.pow(a,t,n)             
+       while t != n-1 && y != 1 && y != n-1
+         y = (y * y) % n
+         t <<= 1
+       end
+       return false if y != n-1 && t & 1 == 0
+     end
+     return true
+   end
+
+end
+ 
+module ModMath
+
+   def ModMath.pow(base, power, mod)
+     result = 1
+     while power > 0
+       result = (result * base) % mod if power & 1 == 1
+       base = (base * base) % mod
+       power >>= 1;
+     end
+     result
+   end
+
+end
+
+class GCD
+
+	def gcd(a, b)
+		if b == 0
+			#puts a
+			return a
+		else
+			gcd(b, a % b)
+		end
+	end
+end
+
+# Calculate ((b**p) % m) assuming that b and m are large integers.
+def Math.powmod(b, p, m)
+	if p == 1
+		b % m
+	elsif (p & 0x1) == 0 # p.even?
+		t = powmod(b, p >> 1, m)
+		(t * t) % m
+	else
+		(b * powmod(b, p-1, m)) % m
+	end
+end
+
+class Jacobian
+
+	def findRepeat(p)
+		j = Jacobian.new
+
+		for i in (1..25)
+			if(j.find(p) == false)
+				return false
+			end
+		end
+
+		return true
+	end
+
+
+	def find(p)
+		r = rand(p-1)
+		r = r + 1
+
+		j = Jacobian.new
+		return j.jacobian(r, p)
+	end
+
+	def jacobian(r, p)
+
+		gcd = GCD.new
+		j = Jacobian.new
+
+		# Test 1
+		if(gcd.gcd(r, p) == 1)
+			# Test 2
+			# Compute LHS of equation
+			lhs = j.jacobianLHS(r, p)
+			rhs = j.jacobianRHS(r, p)
+			return j.isCongruent(lhs, rhs, p)
+		else
+			return false
+		end
+
+	end
+
+	def isCongruent(b, c, m)
+		x = ((b-c)/m)
+		return x.is_a?(Integer)
+	end
+
+	def jacobianRHS(r, p)
+		return r ** ((p-1)/2) % p
+		#return r ** ((p-1)/2)
+	end
+
+	def jacobianLHS(r, p)
+		if(r == 1)
+			return 1
+		elsif ((r % 2) == 0)
+			return jacobianLHS((r/2), p) * (-1)**((p**2 -1)/8)
+		elsif ((r % 2) == 1 && r != 1)
+			return jacobianLHS((p % r), r) * (-1)**((r-1)*((p-1)/4))
+		end
+	end
+end
+
+def modinverse(a, z)
+	n = a
+	m = z
+
+	u1 = 1
+	u2 = 0
+	u3 = a
+
+	v1 = 0
+	v2 = 1
+	v3 = z
+
+	while v3 != 0
+		qq = (u3/v3).floor
+		t1 = u1 - qq * v1
+		t2 = u2 - qq * v2
+		t3 = u3 - qq * v3
+		u1 = v1
+		u2 = v2
+		u3 = v3
+		v1 = t1
+		v2 = t2
+		v3 = t3
+		z = 1
+	end
+
+	uu = u1
+	vv = u2
+
+	if vv < 0
+		i = vv + m
+	else
+		i = vv
+	end
+
+	return i
+end
+
+# extended Euclid algorithm
+def findPrivateKey(b,m,_recursion_depth=0)
+	if b % m == 0
+		temp = [0,1]
+		return temp
+	else
+		temp = findPrivateKey(m, b % m, _recursion_depth+1)
+		temp2 = [temp[1], temp[0]-temp[1] * ((b/m).to_i)]
+		if _recursion_depth == 0
+			return temp2[0] % m
+		else
+			return temp2
+		end
+	end
+end
+
+class Encryption
+
+	def encrypt(n, e, m)
+		return Math.powmod(m.to_i, e.to_i, n.to_i)
+	end
+
+	def decrypt(n, d, m)
+		return Math.powmod(m, d, n)
+	end
+
+end
--- labs/lab3/code/server.rb
+++ labs/lab3/code/server.rb
@@ -1,24 +1,75 @@
 #! /usr/bin/env ruby -w
 
 require "socket"
-load "client.rb"
+load "myLib.rb"
+#load "client.rb"
 
 def echo_server(n)
-	ssock = TCPserver.open('127.0.0.1', 0)
+	e = Encryption.new
+
+	ssock = TCPserver.open('127.0.0.1', 8080)
+
 	port = ssock.addr[1]
 	if pid = fork then
 		# parent is server
 		csock = ssock.accept
+
 		n = 0
+
 		while str = csock.gets
+		cyphertext = str.to_i
+		puts "[SERVER] Received: #{cyphertext}\n"
+
+		#plaintext = e.decrypt(6085620532976717, 10142701089716483, cyphertext)
+		plaintext = e.decrypt(10142701089716483, 6085620532976717, cyphertext)
+		puts "[SERVER] Plaintext: #{plaintext}\n"
+
+		cyphertext_new = e.encrypt(10142789312725007, 5, plaintext)
+		puts "[SERVER] Cyphertext: #{cyphertext_new}\n"
+
+
+
 			n += csock.write(str)
 		end
+
 		Process.wait
-		printf "server processed %d bytes\n", n
+
 	else
 		# child is client
 		echo_client(n, port)
+		return
 	end
 end
+DATA = 34923459234099
 
+def echo_client(n, port = 8081)
+	sock = TCPsocket.open('127.0.0.1', port)
+
+	puts "[CLIENT] Plaintext: #{DATA}\n"
+
+	e = Encryption.new
+
+	cyphertext = e.encrypt(10142701089716483, 5, DATA)
+	puts "[CLIENT] Cyphertext: #{cyphertext}\n"
+
+
+	cyphertext_s = cyphertext.to_s + "\n"
+	n.times do
+		sock.write(cyphertext_s)
+		ans = sock.readline
+	puts "[CLIENT] Cyphertext From Server: #{ans}\n"
+
+	plaintext = e.decrypt(10142789312725007, 8114231289041741, ans.to_i)
+	puts "[CLIENT] Plaintext From Server: #{plaintext}\n"
+
+
+
+	end
+
+	sock.close
+
+end
+
+#echo_server()
 echo_server(Integer(ARGV.shift || 1))
+#respond_server(Integer(ARGV.shift || 1))
