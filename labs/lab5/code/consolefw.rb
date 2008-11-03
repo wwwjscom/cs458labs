e4d67a55b3f0e3cad87c85c5444e8aed79b4f2ee
Jason Soo <wwwjscom@gmail.com>
2008-11-03 16:53:22 -0600

    Looks finished...a few bugs I know exist but oh wells

--- labs/lab5/code/consolefw.rb
+++ labs/lab5/code/consolefw.rb
@@ -1,6 +1,7 @@
 #! /usr/bin/env ruby -w
 
-@@rulesArray = Array.new{}
+@run = true
+@@rulesArray = Array.new
 
 def addRule
 
@@ -42,7 +43,82 @@ def addRule
 			"action" => action
 		}
 
-	@@rulesArray.push(h)
+
+	if validRule?(h) then
+		@@rulesArray.push(h)
+		puts "Rule added!"
+	end
+	#puts validRule?(h) ? "Rule added!" : "Error"
+end
+
+def validRule? rule
+
+	src_ip 		= rule.fetch('src_ip')
+	src_netmask 	= rule.fetch('src_netmask')
+	src_port 	= rule.fetch('src_port')
+
+	dest_ip 	= rule.fetch('dest_ip')
+	dest_netmask 	= rule.fetch('dest_netmask')
+	dest_port 	= rule.fetch('dest_port')
+
+	protocol 	= rule.fetch('protocol')
+	action 		= rule.fetch('action')
+
+	@isOK = true
+	@errMsg = ""
+
+	if src_ip < 1
+		@isOK = false
+		@errMsg << "Source IP is invalid\n"
+	end
+
+	if src_netmask < 1
+		@isOK = false
+		@errMsg << "Source Mask is invalid\n"
+	end
+
+	if src_port < 1
+		@isOK = false
+		@errMsg << "Source Port is invalid\n"
+	end
+
+	if dest_ip < 1
+		@isOK = false
+		@errMsg << "Dest IP is invalid\n"
+	end
+
+	if dest_netmask < 1
+		@isOK = false
+		@errMsg << "Dest Mask is invalid\n"
+	end
+
+	if dest_port < 1
+		@isOK = false
+		@errMsg << "Dest Port is invalid\n"
+	end
+
+	#if protocol != 0 or protocol != 1 or protocol != 6 or protocol != 17
+	#	@isOK = false
+	#	@errMsg << "Protocol is invalid\n"
+	#end
+
+	if action < 0 or action > 1
+		@isOK = false
+		@errMsg << "Action is invalid\n"
+	end
+	# If there was an error
+	if @isOK == false
+		puts "Error! Unable to add rule!"
+		puts @errMsg
+		return false
+	else
+		return true
+	end
+
+end
+
+def numeric?(object)
+	true if Float(object) rescue false
 end
 
 # Loop through the rules Array and print
@@ -52,6 +128,12 @@ def printRules
 	@@rulesArray.each do |rule|
 		puts "#{@i}-#{rule.fetch('src_ip')}/#{rule.fetch('src_netmask')}:#{rule.fetch('src_port')} #{rule.fetch('dest_ip')}/#{rule.fetch('dest_netmask')}:#{rule.fetch('dest_port')} #{rule.fetch('protocol')} #{rule.fetch('action')}"
 		@i += 1
+	end
+
+	if @i == 1
+		puts "No rules to print!"
+	else
+		puts "Rules Loaded!"
 	end
 end
 
@@ -67,29 +149,35 @@ def writeFile
 	end
 
 	outputFile.close
+
+	puts "Rules Saved!"
 end
 
 def loadRules
 
+	@i = 1
+
 	File.open("./rules").each do |line|
 		
-		ip = line.slice!(/.+-/)[0..-2]
+		line = line.chop
 
-		src_ip	= line.slice!(/\w*\//)[0..-2]
+		ip = line.slice!(/.+-/)[0..-2].to_i
 
-		src_netmask = line.slice!(/\w+:/)[0..-2]
+		src_ip	= line.slice!(/\w*\//)[0..-2].to_i
 
-		src_port = line.slice!(/\w+ /)[0..-2]
+		src_netmask = line.slice!(/\w+:/)[0..-2].to_i
 
-		dest_ip = line.slice!(/\w+\//)[0..-2]
+		src_port = line.slice!(/\w+ /)[0..-2].to_i
 
-		dest_netmask = line.slice!(/\w+:/)[0..-2]
+		dest_ip = line.slice!(/\w+\//)[0..-2].to_i
 
-		dest_port = line.slice!(/\w+ /)[0..-2]
+		dest_netmask = line.slice!(/\w+:/)[0..-2].to_i
 
-		protocol = line.slice!(/\w+ /)[0..-2]
+		dest_port = line.slice!(/\w+ /)[0..-2].to_i
 
-		action = line
+		protocol = line.slice!(/\w+ /)[0..-2].to_i
+
+		action = line.to_i
 
 		h = Hash.new{}
 		h =
@@ -104,8 +192,16 @@ def loadRules
 				"action" => action
 			}
 
-		@@rulesArray.push(h)
+		if validRule?(h) 
+			@@rulesArray.push(h)
+		else
+			puts "Error parsing line #{@i}"
+		end
+
+		@i += 1
 	end
+
+	puts "Rules Loaded!"
 end
 
 def deleteRules
@@ -117,15 +213,55 @@ def deleteRules
 		# Delete the given rule
 		@@rulesArray.delete_at((rule.to_i)-1)
 	end
+
+	puts "Rule deleted!"
 end
 
-while true
+def startFirewall
+	cmd = "ipfw -q add"
+
+	# flush
+	system('ipfw -q -f flush')
+
+	@i = 0
+	@@rulesArray.each do |rule|
+
+		sip = rule.fetch('src_ip')
+		dip = rule.fetch('dest_ip')
+		sport = rule.fetch('src_port')
+		action = rule.fetch('action')
+		protocol = rule.fetch('protocol')
+
+		# Build the rule itself
+		feed = "#{action} #{protocol} from #{dip} to #{sip} #{sport}"
+
+		# Build the complete rule
+		exec = "#{cmd} #{feed}"
+
+		# Add the rule
+		system(exec)
+
+		@i += 1
+	end
+
+	# start the firewall
+	system('sh /usr/local/etc/ipfw.rules')
+
+	puts "Firewall Started!"
+end
+
+def stopFirewall
+	#system('ipfw -q -f flush')
+	puts "Firewall Stopped!"
+end
+
+while @run
 
 	puts "1. Add Rule\n"
 	puts "2. Delete Rule\n"
 	puts "3. Print Rules\n"
 	puts "4. Start Firewall\n"
-	puts "5. Save Firewall\n"
+	puts "5. Stop Firewall\n"
 	puts "6. Save Rules To File\n"
 	puts "7. Load Rules From File\n"
 	puts "8. Quit\n"
@@ -137,7 +273,12 @@ while true
 		when 1 then addRule
 		when 2 then deleteRules
 		when 3 then printRules
+		when 4 then startFirewall
+		when 5 then stopFirewall
 		when 6 then writeFile
 		when 7 then loadRules
+		when 8 then
+			puts "Bye now!" 
+			@run = false 
 	end
 end
