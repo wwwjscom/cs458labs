15e49fe3eb5ada2f5a813892db69c8d783e7c917
Jason Soo <wwwjscom@gmail.com>
2008-11-10 16:30:32 -0600

    Setup the APIs for part 1

---
+++ labs/lab6/code/snifall.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/env ruby -w
+
+# Parses a dumped line from tcpdump
+def parseDumpLine line
+end
+
+# Runs the tcpdump
+# Probably needs to be threaded
+# and have its output redirected
+def dump
+end
+
+def setInterface i
+end
+
+def getInterface
+end
+
+# formats the output line
+def output
+end
+
+def main
+end
