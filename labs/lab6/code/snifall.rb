3e713f77f6329a2074ccf9fc5da083d8d05d3ba8
Jason Soo <wwwjscom@gmail.com>
2008-11-11 14:56:11 -0600

    3.1 complete

--- labs/lab6/code/snifall.rb
+++ labs/lab6/code/snifall.rb
@@ -1,49 +1,9 @@
 #!/usr/bin/env ruby -w
 
-# Parses a dumped line from tcpdump
-def parseDumpLine line
+require 'pcaplet'
+
+network = Pcaplet.new('-n -i en1')
+
+network.each_packet do |pkt|
+	puts "#{pkt.ip_src}:#{pkt.sport} #{pkt.ip_dst}:#{pkt.dport}"
 end
-
-# Runs the tcpdump
-# Probably needs to be threaded
-# and have its output redirected
-def dump
-	@@thread = Thread.new do
-		system("tcpdump -n -i en1")
-	end
-end
-
-def setInterface i
-end
-
-def getInterface
-end
-
-# formats the output line
-def output
-end
-
-def laters
-	Thread.kill(@@thread)
-end
-
-
-def main
-	@run = true
-	setInterface "en1"
-
-	dump
-
-	command = gets.to_i
-
-	while @run
-		case command
-			when 0 then 
-				laters
-				@run = false
-		end
-	end
-
-end
-# kick off program
-main
